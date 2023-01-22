7accc47f6c6a16fd165f3ce1cca77a0ce8d8e8be
Larissa <contatolarisn@gmail.com>
2023-01-21 23:06:08 -0300

    final commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 const botaoTrailer = document.querySelector(".botao-trailer");
-
 const botaoFecharModal = document.querySelector(".fechar-modal");
-
 const video = document.getElementById("video");
-
 const modal = document.querySelector(".modal");
-
 const linkDoVideo = video.src;
 
 
